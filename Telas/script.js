958184352db5f4d2efffb172358f4e8beb1d41eb
EdvaldoDantas <135360521+EdvaldoDantas@users.noreply.github.com>
2024-02-22 16:27:43 +0100

    img

--- Telas/script.js
+++ Telas/script.js
@@ -1,14 +1,7 @@
 const x =  document.getElementById('subir')
 const y = document.getElementById('descer')
 window.addEventListener("resize", ()=>{
-    if(window.innerWidth < 250){
-        x.style.display = 'none'
-       y.style.display = 'none' 
-    }
-    else{
-       x.style.display = 'block'
-       y.style.display = 'block'  
-    }
+   
 })
 x.addEventListener('click', ()=>{
     y.style.display = 'none'
